31ac8148c1f1b6f5e47852c23613dd3c622e7a98
Kazu <kazuki.ino01@gmail.com>
2025-12-31 20:13:07 +0900

    renderで起動できるように編集

--- index.js
+++ index.js
@@ -74,7 +74,8 @@ const server = http
   .on('clientError', e => { // クライアント側のエラー
     console.error(`[${new Date()}] Client Error`, e);
   });
-const port = 8000;
+
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
