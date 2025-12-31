246156f6dcae7cbb98cfd2e546cbd60c8850a54c
Kazu <kazuki.ino01@gmail.com>
2025-12-31 16:09:15 +0900

    アクセスログをコンソールに出力する機能を実装

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 'use strict';
 const http = require('node:http');
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { // 200→OK
-    'Content-Type': 'text/html; charset=utf-8'
+const server = http
+  .createServer((req, res) => {
+    console.info( // アクセスログをコンソールに出力
+      `[${new Date()}] Requested by ${req.socket.remoteAddress}`
+    );
+    res.writeHead(200, { // 200→OK
+      'Content-Type': 'text/plain; charset=utf-8'
+    });
+    res.write(req.headers['user-agent']);
+    res.end();
+  })
+  .on('error', e => { // サーバー側のエラー
+    console.error(`[${new Date()}] Server Error`, e);
+  })
+  .on('clientError', e => { // クライアント側のエラー
+    console.error(`[${new Date()}] Client Error`, e);
   });
-  res.write(`<!DOCTYPE html>
-<html lang="ja">
-  <body>
-    <h1>HTMLの一番大きい見出しを表示します</h1>
-  </body>
-</html>`);
-  res.end();
-});
-
 const port = 8000;
 server.listen(port, () => {
   console.log(`Listening on ${port}`);
