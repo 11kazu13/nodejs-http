1f5c3327d8843c4dcc2c84f622b101a6146cbb9c
Kazu <kazuki.ino01@gmail.com>
2025-12-31 16:34:52 +0900

    get/postメソッドを実装

--- index.js
+++ index.js
@@ -2,13 +2,32 @@
 const http = require('node:http');
 const server = http
   .createServer((req, res) => {
+    const now = new Date();
     console.info( // アクセスログをコンソールに出力
-      `[${new Date()}] Requested by ${req.socket.remoteAddress}`
+      `[${now}] Requested by ${req.socket.remoteAddress}`
     );
     res.writeHead(200, { // 200→OK
       'Content-Type': 'text/plain; charset=utf-8'
     });
-    res.write(req.headers['user-agent']);
+
+    switch (req.method) {
+      case 'GET':
+        res.write(`GET でアクセスされました ${req.url}`);
+        break;
+      case 'POST':
+        res.write(`POST でアクセスされました ${req.url}`);
+        let rawData = '';
+        req
+          .on('data', chunk => {
+            rawData += chunk;
+          })
+          .on('end', () => {
+            console.info(`[${now}] Data posted: ${rawData}`);
+          });
+        break;
+        default:
+          break;
+    }
     res.end();
   })
   .on('error', e => { // サーバー側のエラー
